1c87b7ede3dd11cf9d87ea75b77ab3910932c01b
chitsanuphongKie <chitsanuphongge@gmail.com>
2023-02-21 13:49:02 +0700

    add javascript style guide

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,29 +1,14 @@
-import React, {useState, useEffect} from "react";
-import { fetchTodo } from "../api/todo.api";
+import { computeHeadingLevel } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
 
 function Homepage() {
-  const [dataPost, setDataPost] = useState([]);
+  
 
-  useEffect(() => {
-    getTodo()
-  }, []);
-
-  const getTodo = async () => {
-    let tmpdata = await fetchTodo()
-    setDataPost(tmpdata)
-  }
   return (
     <div className="relative flex items-center justify-center">
-      {/* <select name="" id="">
-        <option value="1" disabled hidden>เลือกข้อมูล</option>
-        {dataPost.map((item, index) => {
-          return (
-            <option value={item.id} key={index}>
-              {item.title}
-            </option>
-          );
-        })}
-      </select> */}
+      <div>
+        
+      </div>
     </div>
   );
 }
